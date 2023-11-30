a201421a9de8da4ff41bfb09b1f4750a20b076a1
Victor <vidrums86@gmail.com>
2023-11-30 10:47:30 +0000

    formik dev

--- client/src/Components/Form/Components/Input/Input.jsx
+++ client/src/Components/Form/Components/Input/Input.jsx
@@ -1,28 +1,31 @@
-import { Field, ErrorMessage } from 'formik';
+import { Field, ErrorMessage } from "formik";
 import PropTypes from "prop-types";
 
-import './Input.scss';
+import "./Input.scss";
 
-
-const Input = ({ type, placeholder, name,}) => {
-
-	return (
-        <div>
-            <Field type={type} className="form-input" name={name} placeholder={placeholder}/>
-            <ErrorMessage className='error-message' name={name} component='p'/>
-        </div>
-	)
-}
+const Input = ({ type, placeholder, name }) => {
+  return (
+    <div>
+      <Field
+        type={type}
+        className="form-input"
+        name={name}
+        placeholder={placeholder}
+      />
+      <ErrorMessage className="error-message" name={name} component="p" />
+    </div>
+  );
+};
 
 Input.defaultProps = {
-    type:'text'
-}
-  
-Input.propTypes = {
-    type: PropTypes.string,
-    placeholder: PropTypes.string,
-    name: PropTypes.string,
-    className: PropTypes.string,
-}
+  type: "text",
+};
 
-export default Input
+Input.propTypes = {
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  name: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default Input;
--- client/src/Components/Form/Components/UserForm.jsx
+++ client/src/Components/Form/Components/UserForm.jsx
@@ -15,69 +15,80 @@ const UserForm = () => {
 
   const formInfo = (values) => {
     const userCartInfo = JSON.parse(localStorage.getItem("cart"));
-    console.log(values, userCartInfo);
+    console.log(userCartInfo);
     dispatch(formClose());
     dispatch(modalSubmitOpen());
   };
 
   return (
-    <div className="form-wrapper">
-      <Formik
-        initialValues={{
-          name: "",
-          lastName: "",
-          age: "",
-          address: "",
-          phone: "",
+    <div className="form-container">
+      <div
+        className="form-wrapper"
+        onClick={(e) => {
+          e.stopPropagation();
         }}
-        onSubmit={(values) => formInfo(values)}
-        validationSchema={validationSchema}
       >
-        {({ errors, touched }) => (
-          <Form>
-            <RxCross2
-              className="close-icon"
-              onClick={() => {
-                dispatch(formClose());
-              }}
-            />
-            <header className="form-header">
-              <h2 className="form-title">Please,fill the form</h2>
-            </header>
-            <Input
-              name="name"
-              placeholder="Name"
-              error={errors.name && touched.name}
-              type="text"
-            />
-            <Input
-              name="lastName"
-              placeholder="Last Name"
-              error={errors.lastName && touched.lastName}
-              type="text"
-            />
-            <Input
-              name="age"
-              placeholder="age"
-              error={errors.age && touched.age}
-              type="number"
-            />
-            <Input
-              name="address"
-              placeholder="address"
-              error={errors.address && touched.address}
-              type="text"
-            />
-            <Input
-              name="phone"
-              placeholder="phone number"
-              error={errors.phone && touched.phone}
-              type="tel"
-            />
-            <ConfirmButton text="Submit" type="submit" className="submit-btn" />
-          </Form>
-        )}
-      </Formik>
+        <Formik
+          initialValues={{
+            name: "",
+            lastName: "",
+            age: "",
+            address: "",
+            phone: "",
+          }}
+          onSubmit={(values) => formInfo(values)}
+          validationSchema={validationSchema}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <RxCross2
+                className="close-icon"
+                onClick={() => {
+                  dispatch(formClose());
+                }}
+              />
+
+              <h1 className="form-title">Please, fill the form</h1>
+
+              <Input
+                name="name"
+                placeholder="Name"
+                error={errors.name && touched.name}
+                type="text"
+              />
+              <Input
+                name="lastName"
+                placeholder="Last Name"
+                error={errors.lastName && touched.lastName}
+                type="text"
+              />
+              <Input
+                name="age"
+                placeholder="age"
+                error={errors.age && touched.age}
+                type="number"
+              />
+              <Input
+                name="address"
+                placeholder="address"
+                error={errors.address && touched.address}
+                type="text"
+              />
+              <Input
+                name="phone"
+                placeholder="phone number"
+                error={errors.phone && touched.phone}
+                type="tel"
+              />
+              <ConfirmButton
+                text="Submit"
+                type="submit"
+                className="confirm-btn"
+              />
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
