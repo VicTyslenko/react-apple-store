6a8f84e9d645b53e67b3468a727d6ddb13dd02f1
Victor <vidrums86@gmail.com>
2025-06-03 21:01:18 +0100

    upd: removed docker files

--- server/routers/users.api.js
+++ server/routers/users.api.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 const router = Router();
-// const { v4: uuidv4 } = require("uuid");
 
 const User = require("../modules/users.mongoose");
 
@@ -13,22 +12,17 @@ router.post("/register", async (request, response) => {
     age: age,
     address: address,
     phone: phone,
-    // password: password,
-    // tokenUser: uuidv4(),
   };
 
   try {
     const user = new User(newUser);
     await user.save();
+
     response.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     if (error.code === 11000) {
       const field = Object.keys(error.keyPattern)[0];
-      return response
-        .status(400)
-        .json(
-          `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`
-        );
+      return response.status(400).json(`${field.charAt(0).toUpperCase() + field.slice(1)} already exists`);
     }
     response.status(500).json({ error: error.message });
   }
